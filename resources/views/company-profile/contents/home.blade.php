231fd0f6197776858342209686036d0fbf0f1e35
faizhermawan_ <faizhermawan13@gmail.com>
2023-06-10 23:42:19 +0700

    create home page and styling

--- resources/views/company-profile/contents/home.blade.php
+++ resources/views/company-profile/contents/home.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Driving Courses</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+    <link href="assets/css/style.css" rel="stylesheet">
   </head>
   <body>
 
@@ -35,18 +37,32 @@
               <a class="nav-link active" aria-current="page" href="#">Contact Us</a>
             </li>
           </ul>
-            <li class="d-flex nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Not Signed In
-              </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Login</a></li>
-                <li><a class="dropdown-item" href="#">Sign Up</a></li>
-              </ul>
-            </li>
         </div>
       </div>
     </nav>
+
+    <!-- ======= Hero Section ======= -->
+  <section id="hero" class="hero d-flex align-items-center">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 d-flex flex-column justify-content-center">
+          <h1 data-aos="fade-up">Drive with Confidence, Navigate Life's Roads!</h1>
+          <h3 data-aos="fade-up" data-aos-delay="200">Join now and enjoy a fun learning experience guided by experienced instructors</h3>
+          <div data-aos="fade-up" data-aos-delay="600">
+            <div class="text-center text-lg-start">
+              <a href="#job" class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
+                <span>More info</span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6 hero-img" data-aos="zoom-out" data-aos-delay="200">
+          <img src="{{asset('assets/images/home-car.svg')}}" class="img-fluid" alt="" width="400px">
+        </div>
+      </div>
+    </div>
+
+  </section><!-- End Hero -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
   </body>
